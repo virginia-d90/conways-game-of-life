4c31c703e36490c3ac35554bdc705e09e568cc00
Virginia Davenport <virginialdport@gmail.com>
2020-08-25 10:46:41 -0600

    added rules and header

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Rules from "./components/Rules";
+
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Rules />
+      
+    </div>
+  );
+}
+
+export default App;
