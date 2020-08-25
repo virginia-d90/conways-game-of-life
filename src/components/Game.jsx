9b8f4c8d70d43957f4a6a10191371cad6595330d
Virginia Davenport <virginialdport@gmail.com>
2020-08-25 16:32:19 -0600

    basics are working, having trouble with a generation counter

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,19 +4,77 @@ import produce from "immer";
 const numRows = 25;
 const numCols = 40;
 
+const neighborLoc = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+]
+
+
 const generateEmptyGrid = () => {
     const rows = [];
+    
     for(let i = 0; i < numRows; i++){
         rows.push(Array.from(Array(numCols), () => 0));
     }
+
     return rows;
+    
 }
 
 function Game(){
+    // setting state
     const [play, setPlay] = useState(false);
     const [grid, setGrid] = useState(() => {
         return generateEmptyGrid();
     })
+    const [gen, setGen] = useState(0)
+    
+
+    const playRef = useRef(play);
+    playRef.current = play
+
+    const playing = useCallback(() => {
+        if(!playRef.current){
+            return
+        }
+
+       
+       
+        setGrid((oldGrid) => {
+            
+            return produce(oldGrid, gridCopy => {
+                for(let i=0; i < numRows; i++){
+                    for(let j=0; j < numCols; j++){
+                        let nb = 0;
+                        neighborLoc.forEach(([x, y]) => {
+                            const nI = i + x;
+                            const nJ = j + y;
+
+                            if(nI >= 0 && nI < numRows && nJ >= 0 && nJ < numCols){
+                                nb += oldGrid[nI][nJ]
+                            }
+                        })
+                        if(nb < 2 || nb > 3){
+                            gridCopy[i][j] = 0;
+                        } else if(oldGrid[i][j] === 0 && nb === 3){
+                            gridCopy[i][j] = 1
+                        }
+                    }  
+                }
+            })
+        })
+
+        
+        setTimeout(playing, 100)
+    }, [])
+
+   
 
     
     return(
@@ -49,7 +107,8 @@ function Game(){
 
             </div>
             <div className="buttons">
-            <button>{play ? "Stop" : "Start"}</button>
+            <button onClick={() => {setPlay(true); playRef.current = true; playing()}}>Start</button>
+            <button onClick={() => {setPlay(false); playRef.current = false;}}>Stop</button>
             <button onClick={() => {setGrid(generateEmptyGrid())}}>Reset</button>
             <button onClick={() => {
                 const rows = []
@@ -58,6 +117,7 @@ function Game(){
                 }
                 setGrid(rows)
             }}>Random</button>
+            
             </div>
         </>
     )
